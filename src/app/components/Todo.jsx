28ab02d14a31a7616e1910bda843e9a2daeca6ed
24LP3548 24LP3548 <24LP3548@G304-CC0228.local>
2024-12-30 12:34:47 +0800

    first commit

---
+++ src/app/components/Todo.jsx
@@ -0,0 +1,57 @@
+"use client";
+import { useState } from "react";
+import styles from "./todo.module.css";
+
+export const ToDo = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodoHandler = () => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const DeleteTodo = (index) => {
+    // confirm("Are you sure");
+    setTodos(todos.splice(index + 1, 1));
+    console.log(index);
+  };
+
+  return (
+    <div className={styles.todobackground}>
+      <h1>To-Do list</h1>
+      <div className={styles.inputSection}>
+        <input
+          type="text"
+          placeholder="Add a new task"
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodoHandler}>Add</button>
+      </div>
+      <div className={styles.filterSection}>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+      <div className={styles.todoSection}>
+        {todos.map((todo, index) => {
+          if (todos.length === 0) {
+            return (
+              <div>
+                <p key={index}>Nothing to show</p>
+              </div>
+            );
+          } else
+            return (
+              <div key={index} className={styles.todoContainer}>
+                <div>
+                  <input type="checkbox"></input>
+                  <p>{todo}</p>
+                </div>
+                <button onClick={() => DeleteTodo(index)}>Delete</button>
+              </div>
+            );
+        })}
+      </div>
+    </div>
+  );
+};
