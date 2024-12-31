a794b413a1e5066f59bc7a2ff7e49ab9f4b2f02c
24LP3548 24LP3548 <24LP3548@G304-CC0228.local>
2024-12-31 12:14:01 +0800

    edited day 2

--- src/app/components/Todo.jsx
+++ src/app/components/Todo.jsx
@@ -5,19 +5,22 @@ import styles from "./todo.module.css";
 export const ToDo = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
+  const [activeButton, setActiveButton] = useState("all");
+  const [checkedTasks, setCheckedTasks] = useState(0);
 
   const addTodoHandler = () => {
     setTodos([...todos, newTodo]);
   };
 
   const DeleteTodo = (index) => {
-    // confirm("Are you sure");
+    confirm("Are you sure to delete this item?");
     // setTodos(todos.splice(index + 1, 1));
     // console.log(index);
 
-    const updatedTodos = [...todos.slice(0, index), ...todos.slice(index + 1)];
-    setTodos(updatedTodos);
+    setTodos([...todos.slice(0, index), ...todos.slice(index + 1)]);
   };
+
+  // const completedTask2
 
   return (
     <div className={styles.todobackground}>
@@ -31,9 +34,24 @@ export const ToDo = () => {
         <button onClick={addTodoHandler}>Add</button>
       </div>
       <div className={styles.filterSection}>
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+        <button
+          className={activeButton == "all" && styles.activeButton}
+          onClick={() => setActiveButton("all")}
+        >
+          All
+        </button>
+        <button
+          className={activeButton == "active" && styles.activeButton}
+          onClick={() => setActiveButton("active")}
+        >
+          Active
+        </button>
+        <button
+          className={activeButton == "completed" && styles.activeButton}
+          onClick={() => setActiveButton("completed")}
+        >
+          Completed
+        </button>
       </div>
       <div className={styles.todoSection}>
         {todos.map((todo, index) => {
@@ -47,13 +65,16 @@ export const ToDo = () => {
             return (
               <div key={index} className={styles.todoContainer}>
                 <div>
-                  <input type="checkbox"></input>
+                  <input type="checkbox" onClick={completedTask()}></input>
                   <p>{todo}</p>
                 </div>
                 <button onClick={() => DeleteTodo(index)}>Delete</button>
               </div>
             );
         })}
+      </div>
+      <div className={styles.footer}>
+        <p>1 of {todos.length} tasks completed</p>
       </div>
     </div>
   );
